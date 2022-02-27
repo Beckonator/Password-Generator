9fca8ddceb1ffebf935f081d043016c1fc91c6e8
Chandler Beck <beckonator25@gmail.com>
2022-02-27 15:15:56 -0700

    Generated Password Function

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,25 @@
 // Assignment code here
+var characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+var passwordLength = ;
+var passwordValue = "";
 
-
-// Get references to the #generate element
+// Get references to the element
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function getPassword() {
+  var promptCriteria = window.prompt("Include Uppercase? 'Yes' or 'No' .")
+  passwordValue = "";
 
-  passwordText.value = password;
+  for (var i = 0; i < passwordLength; i++) {
+    var number = Math.floor(Math.random() * characters.length);
+    passwordValue += characters.substring(number, number + 1);
+  }
 
+  passwordText.value = passwordValue;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getPassword);
